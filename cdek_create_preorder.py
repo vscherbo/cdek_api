cc5f246f5f6d0724b2cb9e4a67966c8fefc0332c
Vladimir Scherbo <vscherbo@gmail.com>
2023-11-14 18:07:08 +0300

    for calling within PG function

---
+++ cdek_create_preorder.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Создание предзаказа СДЭК для shp_id
+"""
+
+import logging
+import sys
+import json
+import cdek_api
+
+def main():
+    """
+    Just main proc
+    """
+    #cdek_api.log_app.PARSER.add_argument('--conf', type=str, required=True, help='conf file')
+    #??? cdek_api.log_app.PARSER.set_required('--conf', type=str, required=True, help='conf file')
+    cdek_api.log_app.PARSER.add_argument('--shp', type=int, required=True,
+            help='a shp_id to create an order')
+    args = cdek_api.log_app.PARSER.parse_args()
+    cdek = cdek_api.CDEKApp(args=args)
+    cdek_res = cdek.cdek_shp(args.shp)
+    logging.debug('cdek_res=%s', json.dumps(cdek_res, ensure_ascii=False, indent=4))
+    if cdek.ret_msg is not None:
+        logging.error(cdek.ret_msg)
+        print(cdek.ret_msg, file=sys.stderr, end='', flush=True)
+    else:
+        ent = cdek_res.get('entity')
+        if ent is not None:
+            uuid = ent.get('uuid')
+            if uuid is not None:
+                print(uuid, file=sys.stdout, end='', flush=True)
+            else:
+                print('No key "uuid" in CDEK entity', file=sys.stderr, end='', flush=True)
+        else:
+            print('No key "entity" in CDEK answer', file=sys.stderr, end='', flush=True)
+
+if __name__ == '__main__':
+    main()
