b3b6b725736bf230648973067c4b2cd92e3fbf66
Vladimir Scherbo <vscherbo@gmail.com>
2024-02-13 17:25:06 +0300

    no depends on cdek_preorder_params

--- sql/cr-vw_cdek_to_atol.sql
+++ sql/cr-vw_cdek_to_atol.sql
@@ -6,12 +6,9 @@ AS SELECT prep.prep_id,
     prep.bill_no,
     sb.shp_id
    FROM vw_rcpt_prepayed prep
+     JOIN "Счета" b ON b."№ счета" = prep.bill_no AND b."ДокТК" IS NOT NULL
      JOIN ship_bills sb ON prep.bill_no = sb.bill AND sb.shp_id IS NOT NULL
-     JOIN cdek_preorder_params cpp ON cpp.shp_id = sb.shp_id
-     JOIN cdek_order_status cd ON cd.order_uuid = cpp.cdek_uuid AND cd.code = 'DELIVERED'::character varying
-  WHERE NOT (EXISTS ( SELECT 1
-           FROM atol_rcpt_q
-          WHERE prep.prep_id = atol_rcpt_q.ext_id));
+     JOIN cdek_order_status cd ON cd.cdek_number = b."ДокТК" AND cd.code = 'DELIVERED'::character varying;
 
 -- Permissions
 
