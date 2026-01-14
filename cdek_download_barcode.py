c80f77994757dabf4fa773816e054112418cabff
Владимир Щербо <vscherbo@gmail.com>
2026-01-14 13:49:44 +0300

    no need return of download_barcode()

--- cdek_download_barcode.py
+++ cdek_download_barcode.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """ Загрузка PDF со ШК для заказа СДЭК по uuid-ШК
 """
 
 import logging
 import sys
-import json
+# import json
 import cdek_api
 
 def main():
@@ -17,11 +17,12 @@ def main():
             help='a barcode uuid to download')
     args = cdek_api.log_app.PARSER.parse_args()
     cdek = cdek_api.CDEKApp(args=args)
-    cdek_res = cdek.download_barcode(args.uuid, args.outpdf)
-    #try:
-    #    logging.debug('cdek_res=%s', json.dumps(cdek_res, ensure_ascii=False, indent=4))
-    #except TypeError:
-    #    logging.debug('cdek_res=%s', cdek_res.__dict__)
+    cdek.download_barcode(args.uuid, args.outpdf)
+    # cdek_res = cdek.download_barcode(args.uuid, args.outpdf)
+    # try:
+    #     logging.debug('cdek_res=%s', json.dumps(cdek_res, ensure_ascii=False, indent=4))
+    # except TypeError:
+    #     logging.debug('cdek_res=%s', cdek_res.__dict__)
 
     if cdek.ret_msg is not None and cdek.ret_msg != '':
         logging.error('cdek.ret_msg=%s', cdek.ret_msg)
